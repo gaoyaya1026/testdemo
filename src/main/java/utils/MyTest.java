e5c9e28b896f78516fb11896930fb3054e5ffa5f
gaoyaya <gjx1026@126.com>
2021-03-01 13:50:13 +0800

    pom文件

---
+++ src/main/java/utils/MyTest.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * @program: testdemo
+ * @description:
+ * @author: GYY
+ * @create: 2021-02-23 09:29
+ **/
+public class MyUtil {
+    public static void main() {
+
+    }
+}
